b3aace0bdec08a479c508cf8f31959f1f180f40f
martona <marton@anka.me>
2026-05-04 19:23:33 -0400

    macos

--- src/platform_posix_Settings.h
+++ src/platform_posix_Settings.h
@@ -1,26 +1,26 @@
 #pragma once
 
-bool ReadStringValue(const wchar_t* valueName, std::string& outValue) {
+bool Settings::ReadStringValue(const wchar_t* valueName, std::string& outValue) {
 	//TODO
 	return false;
 }
-bool ReadUint32Value(const wchar_t* valueName, int& outValue) {
+bool Settings::ReadUint32Value(const wchar_t* valueName, int& outValue) {
 	//TODO
 	return false;
 }
-bool WriteStringValue(const wchar_t* valueName, const std::string& value) {
+bool Settings::WriteStringValue(const wchar_t* valueName, const std::string& value) {
 	//TODO
 	return false;
 }
-bool WriteUint32Value(const wchar_t* valueName, int value) {
+bool Settings::WriteUint32Value(const wchar_t* valueName, int value) {
 	//TODO
 	return false;
 }
-bool WriteBinaryValue(const wchar_t* valueName, const unsigned char* data, size_t len) {
+bool Settings::WriteBinaryValue(const wchar_t* valueName, const unsigned char* data, size_t len) {
 	//TODO
 	return false;
 }
-bool ReadBinaryValue(const wchar_t* valueName, std::vector<unsigned char>& outValue) {
+bool Settings::ReadBinaryValue(const wchar_t* valueName, std::vector<unsigned char>& outValue) {
 	//TODO
 	return false;
 }
