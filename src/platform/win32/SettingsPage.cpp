dfaeb024df2334b474972a2a8536e8bbd4bdaed9
marton <marton@anka.me>
2026-05-23 14:29:44 -0400

    windows build fix

--- src/platform/win32/SettingsPage.cpp
+++ src/platform/win32/SettingsPage.cpp
@@ -14,6 +14,7 @@
 #include <winrt/Windows.UI.Text.h>
 #include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.UI.Xaml.Media.h>
 #include <winrt/base.h>
 
