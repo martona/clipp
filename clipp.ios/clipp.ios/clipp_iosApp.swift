ca32676e07feb76173fb18ac1ad1fd47f6818c9b
marton <marton@anka.me>
2026-05-23 01:50:52 -0400

    ios skeleton

---
+++ clipp.ios/clipp.ios/clipp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipp_iosApp.swift
+//  clipp.ios
+//
+//  Created by Marton Anka on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct clipp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
